873addd3a4906b07c9347eedbde39e6230853eae
manalkiyani <manalkiyani687@gmail.com>
2023-11-13 11:27:52 +0500

    added assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,28 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next/types";
+import "./globals.css";
+import { text } from "express";
+
+export const metadata: Metadata = {
+	title: "DevFlow",
+	description: "description here",
+	icons: {
+		icon: "/assets/images/site-logo.svg",
+	},
+};
+
+const inter = Inter({
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "600", "700"],
+	variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
 	children,
@@ -6,9 +30,20 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			appearance={{
+				elements: {
+					formButtonPrimary: "primary-gradient",
+					footerActionLink: `primary-text-gradient
+				hover:text-primary-500`,
+				},
+			}}
+		>
 			<html lang="en">
-				<body>{children}</body>
+				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+					<h1 className="h1-bold">This is a heading to check font</h1>
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	);
